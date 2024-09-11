84cafb9a9a41abe29b5f47e32a01d19749964272
7emerys <153433517+7emerys@users.noreply.github.com>
2024-09-11 16:44:18 +0300

    Add files via upload

---
+++ DKR-6/ДКР 6 МДК-05-02/ДКР 6.pas
@@ -0,0 +1,109 @@
+﻿program QueueImplementation;
+
+const
+  MAX_SIZE = 10;
+
+type
+  Queue = record
+    data: array[1..MAX_SIZE] of Integer;
+    front: Integer;
+    rear: Integer;
+  end;
+
+procedure initQueue(var q: Queue);
+begin
+  q.front := -1;
+  q.rear := -1;
+end;
+
+function isEmpty(var q: Queue): Boolean;
+begin
+  isEmpty := (q.front = -1) and (q.rear = -1);
+end;
+
+function isFull(var q: Queue): Boolean;
+begin
+  isFull := ((q.rear + 1) mod MAX_SIZE = q.front);
+end;
+
+procedure enqueue(var q: Queue; value: Integer);
+begin
+  if isFull(q) then
+    writeln('Очередь переполнена!')
+  else if isEmpty(q) then
+  begin
+    q.front := 1;
+    q.rear := 1;
+    q.data[q.rear] := value;
+  end
+  else
+  begin
+    q.rear := (q.rear mod MAX_SIZE) + 1;
+    q.data[q.rear] := value;
+  end;
+end;
+
+procedure dequeue(var q: Queue);
+begin
+  if isEmpty(q) then
+    writeln('Очередь пуста!')
+  else if q.front = q.rear then
+  begin
+    q.front := -1;
+    q.rear := -1;
+  end
+  else
+    q.front := (q.front mod MAX_SIZE) + 1;
+end;
+
+procedure displayQueue(var q: Queue);
+var
+  i: Integer;
+begin
+  if isEmpty(q) then
+    writeln('Очередь пуста!')
+  else
+  begin
+    writeln('Очередь: ');
+    i := q.front;
+    while i <> q.rear do
+    begin
+      writeln(q.data[i]);
+      i := (i mod MAX_SIZE) + 1;
+    end;
+    writeln(q.data[q.rear]);
+  end;
+end;
+
+var
+  q: Queue;
+  choice, value: Integer;
+
+begin
+  initQueue(q);
+
+  repeat
+    writeln;
+    writeln('Меню:');
+    writeln('1. Добавить в очередь');
+    writeln('2. Убрать из очереди');
+    writeln('3. Показать очередь');
+    writeln('4. Выход');
+    write('Выберите из списка: ');
+    readln(choice);
+
+    case choice of
+      1:
+      begin
+        write('Введите цифру/число для добавления: ');
+        readln(value);
+        enqueue(q, value);
+      end;
+      2: dequeue(q);
+      3: displayQueue(q);
+      4: writeln('Программа завершена');
+    else
+      writeln('Неверное значение!');
+    end;
+  until choice = 4;
+end.
---
+++ DKR-8/ДКР 8 МДК-05-02/Калькулятор простых и сложных процентов/backup/unit1.pas
@@ -0,0 +1,152 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,Math;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    zakr: TBitBtn;
+    prost: TButton;
+    sbros: TButton;
+    sloj: TButton;
+    sum: TEdit;
+    proc: TEdit;
+    kollet: TEdit;
+    p2: TEdit;
+    p3: TEdit;
+    res: TEdit;
+    p4: TEdit;
+    p1: TLabel;
+    pervsum: TLabel;
+    proc1: TLabel;
+    let: TLabel;
+    name1: TStaticText;
+    procedure FormCreate(Sender: TObject);
+    procedure zakrClick(Sender: TObject);
+    procedure prostClick(Sender: TObject);
+    procedure sbrosClick(Sender: TObject);
+    procedure slojClick(Sender: TObject);
+    procedure sumChange(Sender: TObject);
+
+    procedure sumKeyPress(Sender: TObject; var Key: char);
+    procedure procChange(Sender: TObject);
+    procedure procKeyPress(Sender: TObject; var Key: char);
+    procedure kolletChange(Sender: TObject);
+    procedure kolletKeyPress(Sender: TObject; var Key: char);
+    procedure p2Change(Sender: TObject);
+    procedure resChange(Sender: TObject);
+
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+
+procedure TForm1.p2Change(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.resChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.zakrClick(Sender: TObject);
+begin
+  form1.close
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.prostClick(Sender: TObject);
+var x,z:real;
+  y: Real;
+
+  begin
+    if (sum.Text <> '') and (sum.Text[Length(sum.Text)] in ['0'..'9', ')']) then
+       x := StrToFloat(sum.Text);
+    if (proc.Text <> '') and (proc.Text[Length(proc.Text)] in ['0'..'9', ')']) then
+       y := StrToFloat(proc.Text);
+    if (kollet.Text <> '') and (kollet.Text[Length(kollet.Text)] in ['0'..'9', ')']) then
+       z := StrToFloat(kollet.Text);
+    if (res.Text <> '') and (res.Text[Length(res.Text)] in ['0'..'9', ')']) then
+       res.Text := FloatToStr(x + x * y * z / 100);
+  end;
+
+procedure TForm1.sbrosClick(Sender: TObject);
+begin
+  sum.text:='';
+  proc.text:='';
+  kollet.text:='';
+end;
+
+procedure TForm1.slojClick(Sender: TObject);
+var x,z:real;
+  y: Real;
+begin
+  if (sum.text <> '') and (sum.text[Length(sum.text)] in ['0'..'9', ')']) then
+     x:=strtofloat(sum.text);
+  if (proc.text <> '') and (proc.text[Length(proc.text)] in ['0'..'9', ')']) then
+     y:=strtofloat(proc.text);
+  if (kollet.text <> '') and (kollet.text[Length(kollet.text)] in ['0'..'9', ')']) then
+     z:=strtofloat(kollet.text);
+  if (res.text <> '') and (res.text[Length(res.text)] in ['0'..'9', ')']) then
+     res.text := floattostr(round((x*(1+y/100)**z)*100)/100);
+
+end;
+
+procedure TForm1.sumChange(Sender: TObject);
+begin
+
+end;
+
+
+procedure TForm1.sumKeyPress(Sender: TObject; var Key: char);
+begin
+  if not (Key in ['0'..'9', #8])then Key:=#0;
+end;
+
+procedure TForm1.procChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.procKeyPress(Sender: TObject; var Key: char);
+begin
+  if not (Key in ['0'..'9', #8])then Key:=#0;
+end;
+
+procedure TForm1.kolletChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.kolletKeyPress(Sender: TObject; var Key: char);
+begin
+  if not (Key in ['0'..'9', #8])then Key:=#0;
+end;
+
+end.
+
